09c8d9be64255aecac4cad66b1403922d0d256f3
Jeong-ki <7863z@naver.com>
2024-02-27 23:44:36 +0900

    react-native-config env 적용

--- src/api/axios.instance.ts
+++ src/api/axios.instance.ts
@@ -1,11 +1,16 @@
 import axios, {AxiosInstance} from 'axios';
 import {Platform} from 'react-native';
+import Config from 'react-native-config';
 
-export const IOS_BASE_URL = 'http://localhost:3000';
-export const ANDROID_BASE_URL = 'http://10.0.2.2:3000';
+const {BASE_URL, IOS_BASE_URL, ANDROID_BASE_URL} = Config;
+console.log(BASE_URL, Config.IOS_BASE_URL, ANDROID_BASE_URL);
 
 export const api: AxiosInstance = axios.create({
-  baseURL: Platform.OS === 'ios' ? IOS_BASE_URL : ANDROID_BASE_URL,
+  baseURL: !__DEV__
+    ? BASE_URL // FIXME: 백엔드 배포 후 base url만 적용
+    : Platform.OS === 'ios'
+    ? IOS_BASE_URL
+    : ANDROID_BASE_URL,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json; charset=utf-8',
