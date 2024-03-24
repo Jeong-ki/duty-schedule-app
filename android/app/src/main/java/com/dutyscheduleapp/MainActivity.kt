ba3b2f0315d5d22ac49c1f67469ee54e623fc919
Jeong-ki <7863z@naver.com>
2024-03-24 14:35:40 +0900

    fix:앱 이름 변경

--- android/app/src/main/java/com/dutyscheduleapp/MainActivity.kt
+++ android/app/src/main/java/com/dutyscheduleapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.dutyscheduleapp
+package com.multicalendarapp
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -12,7 +12,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "DutyScheduleApp"
+  override fun getMainComponentName(): String = "MultiCalendarApp"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- ios/MultiCalendarAppTests/MultiCalendarAppTests.m
+++ ios/MultiCalendarAppTests/MultiCalendarAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface DutyScheduleAppTests : XCTestCase
+@interface MultiCalendarAppTests : XCTestCase
 
 @end
 
-@implementation DutyScheduleAppTests
+@implementation MultiCalendarAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
