36f32fd45d0d483fd974015e33060bf28e7b502b
Jeong-ki <7863z@naver.com>
2024-03-26 22:47:48 +0900

    update: instance baseURL 경로 설정 방식 변경

--- src/api/axios.instance.ts
+++ src/api/axios.instance.ts
@@ -3,14 +3,15 @@ import {Platform} from 'react-native';
 import Config from 'react-native-config';
 import {setInterceptors} from './axios.interceptors';
 
-const {BASE_URL, IOS_BASE_URL, ANDROID_BASE_URL} = Config;
+const {IOS_BASE_URL, ANDROID_BASE_URL} = Config;
+
+const baseURL = Platform.select({
+  ios: IOS_BASE_URL,
+  android: ANDROID_BASE_URL,
+});
 
 export const api: AxiosInstance = axios.create({
-  baseURL: !__DEV__
-    ? BASE_URL // FIXME: 백엔드 배포 후 base url만 적용
-    : Platform.OS === 'ios'
-    ? IOS_BASE_URL
-    : ANDROID_BASE_URL,
+  baseURL, // FIXME: 백엔드 배포 후 URL 변경
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json; charset=utf-8',
